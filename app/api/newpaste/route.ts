51cac9b67e69a7d48e889e0669b4ca9a8c746e86
Vasanth R <45532566+1337kid@users.noreply.github.com>
2024-10-15 15:03:41 +0530

    Update route.ts

--- app/api/newpaste/route.ts
+++ app/api/newpaste/route.ts
@@ -1,33 +1,20 @@
-import connectToDB from '@utils/database';
-import Paste from '@models/paste';
+import connectToDB from '@utils/database'
+import Paste from '@models/paste'
 
-export const POST = async (req: Request) => {
-  const { title, paste } = await req.json();
-
-  if (title === '') {
-    return new Response(
-      JSON.stringify({ error: 'Title seems to be empty' }),
-      { status: 400, headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' } }
-    );
-  } else if (paste === '') {
-    return new Response(
-      JSON.stringify({ error: 'Paste seems to be empty' }),
-      { status: 400, headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' } }
-    );
-  }
-
+export const POST = async(req:Request) => {
+  const {title,paste} = await req.json()
+  if(title === '') {
+    return new Response(JSON.stringify({error:'Title seems to be empty'}),{status:400})
+  } else if(paste === '') {
+    return new Response(JSON.stringify({error:'Paste seems to be empty'}),{status:400})
+  } 
+  
   try {
     await connectToDB();
-    const newPaste = new Paste({ title, paste });
-    await newPaste.save();
-    return new Response(
-      JSON.stringify({ message: 'OK', pasteID: newPaste._id }),
-      { status: 200, headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' } }
-    );
-  } catch (error) {
-    return new Response(
-      JSON.stringify({ error: 'Error' }),
-      { status: 500, headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' } }
-    );
+    const newPaste = new Paste({title,paste})
+    await newPaste.save()
+    return new Response(JSON.stringify({message:'OK',pasteID:newPaste._id}),{status:200})
+  } catch(error) {
+    return new Response(JSON.stringify({error:'Error'}),{status:500})
   }
-};
+}
