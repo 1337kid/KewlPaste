dd87ca3fe9a4392fa8e8e27ef22805036d0904c6
Pranav M <pranavm265@gmail.com>
2024-10-15 13:27:30 +0530

    Update route.ts

--- app/api/newpaste/route.ts
+++ app/api/newpaste/route.ts
@@ -1,20 +1,33 @@
-import connectToDB from '@utils/database'
-import Paste from '@models/paste'
+import connectToDB from '@utils/database';
+import Paste from '@models/paste';
 
-export const POST = async(req:Request) => {
-  const {title,paste} = await req.json()
-  if(title === '') {
-    return new Response(JSON.stringify({error:'Title seems to be empty'}),{status:400})
-  } else if(paste === '') {
-    return new Response(JSON.stringify({error:'Paste seems to be empty'}),{status:400})
-  } 
-  
+export const POST = async (req: Request) => {
+  const { title, paste } = await req.json();
+
+  if (title === '') {
+    return new Response(
+      JSON.stringify({ error: 'Title seems to be empty' }),
+      { status: 400, headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' } }
+    );
+  } else if (paste === '') {
+    return new Response(
+      JSON.stringify({ error: 'Paste seems to be empty' }),
+      { status: 400, headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' } }
+    );
+  }
+
   try {
     await connectToDB();
-    const newPaste = new Paste({title,paste})
-    await newPaste.save()
-    return new Response(JSON.stringify({message:'OK',pasteID:newPaste._id}),{status:200})
-  } catch(error) {
-    return new Response(JSON.stringify({error:'Error'}),{status:500})
+    const newPaste = new Paste({ title, paste });
+    await newPaste.save();
+    return new Response(
+      JSON.stringify({ message: 'OK', pasteID: newPaste._id }),
+      { status: 200, headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' } }
+    );
+  } catch (error) {
+    return new Response(
+      JSON.stringify({ error: 'Error' }),
+      { status: 500, headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' } }
+    );
   }
-}
+};
